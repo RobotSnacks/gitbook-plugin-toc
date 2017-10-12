b9d1c47bab5f5b87897ebfa7cb6d7fe438047a71
Joshua Toenyes <josh@robotsnacks.io>
2017-10-12 06:17:43 -0600

    Select "h1 + ul" list.

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ module.exports = {
 			});
 			if (this.options.pluginsConfig.toc.addClass) {
 				var className = this.options.pluginsConfig.toc.className || 'toc';
-				page.content = page.content + '\n\n\n<script type="text/javascript">var targetUl = document.getElementsByClassName(\'page-inner\')[0].getElementsByTagName(\'ul\')[0];if(targetUl.getElementsByTagName(\'a\').length>0){targetUl.className=\'' + className + '\';}</script>';
+				page.content = page.content + '\n\n\n<script type="text/javascript">var targetUl = document.querySelector("h1 + ul");if(targetUl.getElementsByTagName(\'a\').length>0){targetUl.className=\'' + className + '\';}</script>';
 			}
 			return page;
 		}
